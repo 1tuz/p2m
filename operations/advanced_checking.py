a3cc4b5df0c5bfd96730b8d985c26deed37d2250
1tuz <creameanwonder@gmail.com>
2023-02-27 21:36:18 +0300

    update

--- operations/advanced_checking.py
+++ operations/advanced_checking.py
@@ -4,12 +4,28 @@ from dotenv import load_dotenv
 from params import Params
 
 load_dotenv()
-id_order = input('Type your id_order: ')
-params = Params(id_order=id_order)
+
+id_request = input('Type your id_request: ')
+params = Params(id_request=id_request)
+
+print('*'*24)
+print('****Loading API1...*****')
+print('*'*24)
+
 session = MySession(
-    url=os.getenv('API1_URL1'),
+    url=os.getenv('API1_URL2'),
     username=os.getenv('API_USERNAME'),
     password=os.getenv('API_PASSWORD'),
     params=params.get_params()
 )
 session.print_response()
+
+print('*'*24)
+print('****Loading API2...*****')
+print('*'*24)
+session2 = MySession(
+    url=os.getenv('API2_URL2'),
+    username=os.getenv('API_USERNAME'),
+    password=os.getenv('API_PASSWORD'),
+    params=params.get_params())
+session2.print_response()
