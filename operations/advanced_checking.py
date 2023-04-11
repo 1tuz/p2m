943af0dcd8a064c98677c4c65097ad130a1693e6
1tuz <creameanwonder@gmail.com>
2023-04-11 10:52:12 +0300

    update

--- operations/advanced_checking.py
+++ operations/advanced_checking.py
@@ -3,29 +3,33 @@ import os
 from dotenv import load_dotenv
 from params import Params
 
-load_dotenv()
 
-id_request = input('Type your id_request: ')
-params = Params(id_request=id_request)
+class AdvCheck:
+    load_dotenv()
 
-print('*'*24)
-print('****Loading API1...*****')
-print('*'*24)
+    id_request = input('Type your id_request: ')
+    params = Params(id_request=id_request)
 
-session = MySession(
-    url=os.getenv('API1_URL2'),
-    username=os.getenv('API_USERNAME'),
-    password=os.getenv('API_PASSWORD'),
-    params=params.get_params()
-)
-session.print_response()
+    print('*'*24)
+    print('****Loading API1...*****')
+    print('*'*24)
 
-print('*'*24)
-print('****Loading API2...*****')
-print('*'*24)
-session2 = MySession(
-    url=os.getenv('API2_URL2'),
-    username=os.getenv('API_USERNAME'),
-    password=os.getenv('API_PASSWORD'),
-    params=params.get_params())
-session2.print_response()
+    session = MySession(
+        url=os.getenv('API1_URL2'),
+        username=os.getenv('API_USERNAME'),
+        password=os.getenv('API_PASSWORD'),
+        params=params.get_params()
+    )
+    session.print_response()
+
+    print('*'*24)
+    print('****Loading API2...*****')
+    print('*'*24)
+
+    session2 = MySession(
+        url=os.getenv('API2_URL2'),
+        username=os.getenv('API_USERNAME'),
+        password=os.getenv('API_PASSWORD'),
+        params=params.get_params()
+    )
+    session2.print_response()
